fb881ff429b1be9ba3ada2f6f218f46225332519
unknown <marselina88@list.ru>
2023-03-22 20:43:31 +0300

    Синхронизированные слайдеры

--- src/js/components/slick.js
+++ src/js/components/slick.js
@@ -1,9 +1,26 @@
 import $ from "jquery";
+import Dots from "slick-carousel";
 
 $.when( $.ready ).then(function() {
   $('.header__slider').slick({
+    dots: true, //ЧТОБЫ нижний слайдер не ездил
     infinite: true,
-    slidesToShow: 3,
+    fade: true,
+    prevArrow:  `<img class="slider-arrows slider-arrows__prev" src="img/arrows-prev.svg" alt=""></img>`,
+    nextArrow:  `<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>`,
+    asNavFor: '.slider-dots',
+  });
+
+  $('.slider-dots').slick({
+
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+    //dots: true,
+
+  focusOnSelect: true
+
+
   });
 })
 
