6b53179a9f8da866d6377ffa146719665d18e4bb
unknown <marselina88@list.ru>
2023-03-25 20:28:43 +0300

    video-3, счетчик js плюс - минус
    1.06(https://codepen.io/komarovdesign/pen/PPRbgb)
    input number custom style

--- src/js/components/slick.js
+++ src/js/components/slick.js
@@ -43,16 +43,49 @@ $.when( $.ready ).then(function() {
     fade: true,
     prevArrow:  `<img class="slider-arrows slider-arrows__prev" src="img/arrows-prev.svg" alt=""></img>`,
     nextArrow:  `<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>`,
-  })
+  });
+  $('.holder__slider').slick({
+    infinite: true,
+    fade: true,
+
+    prevArrow:  `<img class="slider-arrows slider-arrows__prev" src="img/arrows-prev.svg" alt=""></img>`,
+    nextArrow:  `<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>`,
+  });
+
+  $('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="img/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="img/minus.svg" alt=""></div></div></div>').insertAfter('.quantity input');
+  $('.quantity').each(function() {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.on('click', function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.on('click', function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+  });
 })
 
 
 
-//$(function() {
 
-  //$(slider).slick({
-   // infinite: true,
-   // slidesToShow: 3,
-   // slidesToScroll: 3
- // })
-//})
